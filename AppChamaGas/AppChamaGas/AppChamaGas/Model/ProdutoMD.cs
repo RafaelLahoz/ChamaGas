a6c76a7dc31a2bd548a0b5a55600ee530a183a42
rafael.ljunior <rafael.ljunior@SJR0577150W10-1>
2019-05-17 21:47:14 -0300

    Commit Inicial

---
+++ AppChamaGas/AppChamaGas/AppChamaGas/Model/ProdutoMD.cs
@@ -0,0 +1,32 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppChamaGas.Model
+{
+    public class ProdutoMD
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [NotNull]
+        public string Descricao { get; set; }
+
+        [NotNull]
+        public decimal Preco { get; set; }
+
+        [NotNull]
+        public int IdFornecedor { get; set; }
+
+        [NotNull]
+        public int IdNuvem { get; set; }
+
+        [NotNull]
+        public DateTime DataAtualizacao { get; set; }
+
+        [NotNull]
+        public string ImgProd { get; set; }
+
+    }
+}
