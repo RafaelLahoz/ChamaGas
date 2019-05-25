854a1a6b886be511cec30b5575ad9e42cb4bfc72
rafael.ljunior <rafael.ljunior@SJR0577150W10-1>
2019-05-24 22:23:35 -0300

    Services 240519

---
+++ AppChamaGas/AppChamaGas/AppChamaGas/Model/Cep.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppChamaGas.Model
+{
+    public class Cep
+    {
+        
+        public string CEP { get; set; }
+                
+        public string Logradouro { get; set; }
+                
+        public string Complemento { get; set; }
+                
+        public string Bairro { get; set; }
+                
+        public string Localidade { get; set; }
+                
+        public string Uf { get; set; }
+                
+        public string Unidade { get; set; }
+                
+        public string Ibge { get; set; }
+                
+        public string Gia { get; set; }
+
+       
+
+
+    }
+}
--- AppChamaGas/AppChamaGas/AppChamaGas/View/PessoaView.xaml.cs
+++ AppChamaGas/AppChamaGas/AppChamaGas/View/PessoaView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AppChamaGas.Model;
+using AppChamaGas.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,34 @@ namespace AppChamaGas.View
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class PessoaView : ContentPage
 	{
-		public PessoaView ()
+        Base_Service client_cep = new Base_Service(Base_Service.URL_VIACEP);
+        //Base_Service client_api = new Base_Service(Base_Service.URL_MINHAAPI);
+
+        public PessoaView ()
 		{
 			InitializeComponent ();
+            
 		}
-	}
+
+        private async void EtCEP_Unfocused(object sender, FocusEventArgs e)
+        {
+            var cep_ret = await client_cep.Get<Cep>(etCEP.Text);
+
+            this.etCEP.Text = cep_ret.CEP;
+            this.etLogradouro.Text = cep_ret.Logradouro;
+            this.etComplemento.Text = cep_ret.Complemento;
+            this.etBairro.Text = cep_ret.Bairro;
+            this.etLocalidade.Text = cep_ret.Localidade;
+            this.etUF.Text = cep_ret.Uf;
+
+            var obj_reqres_user = new ReqRes_Service("users");
+            var user_ret = await obj_reqres_user.Get<RetornoTeste>("2");
+
+            await DisplayAlert("Dados", $"Id: { user_ret.data.FirstOrDefault().id} - Email: { user_ret.data.FirstOrDefault().email}", "Fechar");
+
+
+        }
+
+
+    }
 }
